ee72872aa3e6dcc50dfd8bb8c8f2c7e3b436e201
Sarah Quigley <sarah@sarahquigley.net>
2014-06-05 12:27:50 +0100

    Continue to experiment with scripts on touch devices.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,13 @@ $(function(){
 
   if ( 'ontouchstart' in document.documentElement) {
     $('html').addClass('touch');
+    alert('touch');
   } else {
     $('html').addClass('no-touch');
+    alert('no-touch');
   }
   
-  $("body").swipeRight(function(){
+  $$('body').swipeRight(function(){
     console.log("swipingRight");
     var href = $(".previous").attr('href');
     if( href !== undefined){
@@ -14,12 +16,16 @@ $(function(){
     }
   });
 
-  $("body").swipeLeft(function(){
+  $$('body').swipeLeft(function(){
     console.log("swipingLeft");
     var href = $(".next").attr('href');
     if( href !== undefined){
       window.location.href = href;
     }
+  });
+
+  $$(".post footer").tap(function(){
+    console.log("tapping");
   });
 
 });

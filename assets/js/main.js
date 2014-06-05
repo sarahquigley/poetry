c63e533f6afd1b956c5497aaf1e24c1658735110
Sarah Quigley <sarah@sarahquigley.net>
2014-06-05 09:12:08 +0100

    Experiment with alternative touch library.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,18 +6,16 @@ $(function(){
     $('html').addClass('no-touch');
   }
   
-  $$("body").swipeRight(function(){
+  $("body").swipeRight(function(){
     console.log("swipingRight");
-    alert("swiping right");
     var href = $(".previous").attr('href');
     if( href !== undefined){
       window.location.href = href;
     }
   });
 
-  $$("body").swipeLeft(function(){
+  $("body").swipeLeft(function(){
     console.log("swipingLeft");
-    alert("swiping left");
     var href = $(".next").attr('href');
     if( href !== undefined){
       window.location.href = href;

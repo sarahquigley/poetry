5fec306c363bcf6f646048c740c2e50ba78dc33d
Sarah Quigley <sarah@sarahquigley.net>
2014-06-07 16:20:04 +0100

    Add swiping between poems.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,22 +12,21 @@ $(function(){
     $('html').addClass('no-touch');
   }
 
-  /*
-  $$('body').swipeRight(function(){
-    console.log("swipingRight");
-    var href = $(".previous").attr('href');
-    if( href !== undefined){
-      window.location.href = href;
-    }
-  });
+  $("body").swipe({
+    swipeRight: function(event, distance, duration, fingerCount, fingerData) {
+      var href = $(".previous").attr('href');
+      if( href !== undefined){
+        window.location.href = href;
+      }
+    },
 
-  $$('body').swipeLeft(function(){
-    console.log("swipingLeft");
-    var href = $(".next").attr('href');
-    if( href !== undefined){
-      window.location.href = href;
-    }
+    swipeLeft: function(event, distance, duration, fingerCount, fingerData) {
+      var href = $(".next").attr('href');
+      if( href !== undefined){
+        window.location.href = href;
+      }
+    },
+    threshold:0
   });
-  */
 
 });
